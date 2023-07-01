a8a79674dfae427e167b4e282afbd9d899292ea2
rokysf <135695631+rokysf@users.noreply.github.com>
2023-07-01 02:15:57 +0200

    Add files via upload

---
+++ src/n1exercici2/CalculDni.java
@@ -0,0 +1,15 @@
+package n1exercici2;
+
+public class CalculDni {
+
+	private static String lletrasDni = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+	public static char calcularDni(int numeroDni) {
+
+		int resultat = numeroDni % 23;
+		char lletra = (lletrasDni.charAt(resultat));
+		System.out.println("El DNI complert es: " + numeroDni + lletra);
+		return lletra;
+	}
+
+}
--- test/n1exercici2/DniTest.java
+++ test/n1exercici2/DniTest.java
@@ -1,5 +1,19 @@
 package n1exercici2;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DniTest {
 
+	@ParameterizedTest	
+	@CsvSource({"10345628, K", "37689206, A", "98403769, V", "36225724, B", "45617809, T", 
+			"89256110, X", "32656709, K", "56239078, F", "44674121, W", "36515707, X"})
+
+	public void testLletraDni(int numero, char lletraEsperada) {
+				
+		assertEquals(lletraEsperada, CalculDni.calcularDni(numero));
+	}
+
 }
+
